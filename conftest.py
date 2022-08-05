cce72cc6e9b1ca4f989dfafa39d1f441d4b25542
vsignatov <ignatov.vladimir96@yandex.ru>
2022-08-05 17:57:22 +0300

    import project

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Ввод в консоли сокращенного наименования языка
+ 
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help="Choose lang") 
+ 
+#запуск браузера с добавлением параметра языка 
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    print("\nstart browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+    
