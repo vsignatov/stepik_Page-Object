89a5fd4661b47e74d0937a0af5f83552bf92d6e3
vsignatov <ignatov.vladimir96@yandex.ru>
2022-08-08 12:00:36 +0300

    update project

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,7 @@
-class BasePage ()
+class BasePage ():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
 
-def __init__ (self, browser, url):
-    self.browser = browser
-    self.url = url
-
-def open(self):
-    self.browser.get(self.url)
+    def open(self):
+        self.browser.get(self.url)
